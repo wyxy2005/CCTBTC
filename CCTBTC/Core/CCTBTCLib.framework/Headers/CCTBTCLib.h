6895bc39b252fef418b1f91e779c2aa675ae1018
陈楚涛 <705356202@qq.com>
2019-05-05 11:36:42 +0800

    ..

--- CCTBTC/Core/CCTBTCLib.framework/Headers/CCTBTCLib.h
+++ CCTBTC/Core/CCTBTCLib.framework/Headers/CCTBTCLib.h
@@ -8,11 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-/**
- 完整的 CCTBTCLib 库代码，请加我的QQ：705356202 收费400元（ETH+BTC全套库+Demo代码）
- 代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
- **/
-
 //path类型
 typedef enum Bip44PathType{
     
@@ -78,7 +73,7 @@ typedef enum NetworkType{
               resultBlock:(void(^)(NSArray *array,BOOL suc))block;
 
 /**
- 发起交易
+ 发起交易   需要正式网络，测试网络的交易api已失效
  */
 +(void)TransferByhNetwok:(Network)net
                     Path:(Bip44Path)path
